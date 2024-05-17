130f638d80c8635e864d8217e24007df21e65748
barni10102 <sores.barni@gmail.com>
2024-05-18 01:16:39 +0200

    Image paths corrected and vagrant file added

--- Drumakomi_sivatag/src/main/java/GameView.java
+++ Drumakomi_sivatag/src/main/java/GameView.java
@@ -11,8 +11,8 @@ public class GameView extends JFrame implements ActionListener {
     private JPanel menu;
     private JPanel settings;
     private JButton start;
-    private JButton startgame=new JButton("OK");
-    private JButton back=new JButton("BACK");
+    private JButton startgame = new JButton("OK");
+    private JButton back = new JButton("BACK");
     private TableView table;
     private JTextField playerField;
     private JTextField timeField;
@@ -20,9 +20,10 @@ public class GameView extends JFrame implements ActionListener {
     private int players = 2;
     private int steps = 3;
     private int time = 5;
-    private int xcord =1280;
-    private int ycord =720;
-    private final static String HEGYIMAGE = "\\src\\main\\java\\images\\hegy.png";
+    private int xcord = 1280;
+    private int ycord = 720;
+    private static final String HEGYIMAGE = "src" + File.separator + "main" + File.separator + "java" + File.separator
+            + "images" + File.separator + "hegy.png";
 
     static ArrayList<Image> Pipe = new ArrayList<>();
     static ArrayList<Image> Pump = new ArrayList<>();
@@ -34,33 +35,78 @@ public class GameView extends JFrame implements ActionListener {
 
     static {
         try {
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\cso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\csuszosCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\csuszosRagadosCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\CsuszosRagadosTorottCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\csuszosRagadosTorottVizesCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\csuszosRagadosVizesCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\csuszosTorottCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\csuszosTorottVizescso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\csuszosVizesCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\ragadosCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\ragadosTorottcso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\ragadosTorottVizesCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\ragadosVizesCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\torottCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\torottVizesCso.png").getImage());
-            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\csovek\\vizescso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "cso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "csuszosCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "csuszosRagadosCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "CsuszosRagadosTorottCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "csuszosRagadosTorottVizesCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "csuszosRagadosVizesCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "csuszosTorottCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "csuszosTorottVizescso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "csuszosVizesCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "ragadosCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "ragadosTorottcso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "ragadosTorottVizesCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "ragadosVizesCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "torottCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "torottVizesCso.png").getImage());
+            Pipe.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "csovek" + File.separator
+                    + "vizescso.png").getImage());
 
-            Pump.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\pumpak\\pumpa.png").getImage());
-            Pump.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\pumpak\\destroyedpumpa.png").getImage());
-            Pump.add(new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\pumpak\\vizesPumpa.png").getImage());
+            Pump.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "pumpak" + File.separator
+                    + "pumpa.png").getImage());
+            Pump.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "pumpak" + File.separator
+                    + "destroyedpumpa.png").getImage());
+            Pump.add(new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "pumpak" + File.separator
+                    + "vizesPumpa.png").getImage());
 
-            ciszterna = new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\ciszterna.png").getImage();
-            hegy = new ImageIcon(new File(".").getCanonicalPath() + HEGYIMAGE).getImage();
-            gameBackground = (new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\gameBG.png").getImage());
+            ciszterna = new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator
+                    + "main" + File.separator + "java" + File.separator + "images" + File.separator + "ciszterna.png")
+                    .getImage();
+            hegy = new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "hegy.png").getImage();
+            gameBackground = new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator
+                    + "main" + File.separator + "java" + File.separator + "images" + File.separator + "gameBG.png")
+                    .getImage();
 
-            szerelo = new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\szerelo.png").getImage();
-            szabotor = new ImageIcon(new File(".").getCanonicalPath() + "\\src\\main\\java\\images\\szabotor.png").getImage();
+            szerelo = new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "szerelo.png").getImage();
+            szabotor = new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator + "main"
+                    + File.separator + "java" + File.separator + "images" + File.separator + "szabotor.png").getImage();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -73,7 +119,10 @@ public class GameView extends JFrame implements ActionListener {
         this.setSize(xcord, ycord);
         try {
             this.setIconImage(new ImageIcon(new java.io.File(".").getCanonicalPath() + HEGYIMAGE).getImage());
-            Image img = new ImageIcon(new java.io.File(".").getCanonicalPath() + "\\src\\main\\java\\images\\bg.png").getImage();
+            Image img = new ImageIcon(new File(".").getCanonicalPath() + File.separator + "src" + File.separator
+                    + "main" + File.separator + "java" + File.separator + "images" + File.separator + "bg.png")
+                    .getImage();
+
             menu = new JPanel(null) {
                 public void paintComponent(Graphics g) {
                     super.paintComponent(g);
@@ -97,21 +146,20 @@ public class GameView extends JFrame implements ActionListener {
         this.setVisible(true);
         this.setLocationRelativeTo(null);
     }
+
     public void Settings() {
         this.remove(menu);
         this.repaint();
         try {
             String path = new java.io.File(".").getCanonicalPath();
             this.setIconImage(new ImageIcon(path + HEGYIMAGE).getImage());
-            Image img = new ImageIcon(path + "\\src\\main\\java\\images\\bg.png").getImage();
             settings = new JPanel(new GridLayout(4, 2));
-        }
-            catch (IOException e) {
+        } catch (IOException e) {
             settings = new JPanel(new GridLayout(4, 2));
         }
 
         settings.setVisible(true);
-        settings.setBounds((1280-xcord/2)/2, (720-ycord/2)/2, xcord/2, ycord/2);
+        settings.setBounds((1280 - xcord / 2) / 2, (720 - ycord / 2) / 2, xcord / 2, ycord / 2);
         JLabel player = new JLabel("Set player number: ");
         player.setBounds(0, 0, 640, 180);
         playerField = new JTextField("");
@@ -141,26 +189,27 @@ public class GameView extends JFrame implements ActionListener {
         this.add(settings);
         this.revalidate(); // Add this line to update the frame layout
     }
+
     public void actionPerformed(ActionEvent ae) {
-        if(start.equals(ae.getSource())) {
+        if (start.equals(ae.getSource())) {
             Settings();
         }
-        if(startgame.equals(ae.getSource())) {
+        if (startgame.equals(ae.getSource())) {
             try {
                 players = Integer.parseInt(playerField.getText());
                 steps = Integer.parseInt(stepsField.getText());
                 time = Integer.parseInt(timeField.getText());
+            } catch (NumberFormatException e) {
             }
-            catch (NumberFormatException e){}
             this.remove(settings);
-            table = new TableView(time,steps,players);
+            table = new TableView(time, steps, players);
 
             table.setVisible(true);
             table.setBounds(0, 0, 1280, 720);
             this.add(table);
             this.repaint();
         }
-        if(back.equals(ae.getSource())) {
+        if (back.equals(ae.getSource())) {
             this.dispose();
             new GameView();
         }
